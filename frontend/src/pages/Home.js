0858babdf81205af237651738a813d46ef42f466
vjgarudavigilance <vjgarudavigilance@gmail.com>
2025-02-05 23:10:13 +0530

    added social media icons to the home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,3 @@
-// src/pages/Home.js
 import React from 'react';
 import './Home.css'; // Import the CSS file for Home
 import Cube from '../components/Cube'; // Import the Cube component
@@ -26,8 +25,34 @@ const Home = () => {
         </div>
       </div>
 
-      {/* Include the Carousel below the Cube */}
-       {/* <Carousel />  */}
+      {/* Social Media Icons */}
+      <div className="social-icons">
+        <a
+          href="https://www.linkedin.com/company/vj-garuda-vigilance/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-icon"
+        >
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png"
+            alt="LinkedIn"
+            className="social-icon-img"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/vjgarudavigilance?igsh=MW9nd3hleTczY3h3YQ=="
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-icon"
+        >
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png"
+            alt="Instagram"
+            className="social-icon-img"
+          />
+        </a>
+      </div>
+
     </div>
   );
 };
